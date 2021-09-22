30530773e228edafcf35fa335dd8c76cfd5e3b50
NoheliaEstefhania <71722136+NoheliaEstefhania@users.noreply.github.com>
2021-09-21 23:19:54 -0500

    Timer
    Busqueda secuancial incluyendo el timer

--- Aula_02/E004_BusquedaSecuencial.java
+++ Aula_02/E004_BusquedaSecuencial.java
@@ -6,46 +6,46 @@ import java.util.Scanner;
  * Description: Busqueda Secuencial
  * Date	  : 20/09/2020
  */
+import java.util.Timer;
 
 public class E004_BusquedaSecuencial {
 
 	public static void main(String[] arg) {
-		int n;
 		Scanner s = new Scanner(System.in);
-		int searched;
-		System.out.println("Ingrese la cantidad de números a ingresar :");
-		n=s.nextInt();
-		int [] nums=new int[n];	 
-		System.out.println("----------------");
-		System.out.println("Ingresar los números:");
-		System.out.println("----------------");
-		for(int i=0;i<=nums.length-1;i++) {
-			nums[i]=s.nextInt();		
-		}
-		System.out.println("Ingrese el número que está buscando:");
-		searched=s.nextInt();
-		BusquedaSecuencial(nums, searched);
-	}
-	private static void BusquedaSecuencial(int[]nums, int searched1) {		 
-		int loc=0;
-		while(loc<nums.length && nums[loc] !=searched1)
-			loc++;
-		if (loc==nums.length) 
-			System.out.println("-1");		      
-		else
-			System.out.println("La posición de " + searched1 + " es: " + loc);			 
-	}
-	private void timer() {
 		int[] nums = crearArray();
+		System.out.println("Ingrese el número que está buscando:");
+		int searched=s.nextInt();
+		timer(nums, searched);
+	}
+	private static boolean BusquedaSecuencial(int[]nums, int t) {		 
+		for(int i = 0; i <= nums.length-1; i++) {
+			if (nums[i] == t) {
+
+				return true;
+			}
+		}
+		return false;
+	}
+	private static int[] crearArray() {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Ingrese la cantidad de números del array :");
+		int n=s.nextInt();
+		int x = 0;
+		int [] array= new int [n];
+		for(int i = 0; i <= array.length-1; i++) {
+			array[i]= x;
+			x = x+1;
+
+		}
+		return array;
+	}
+	private static void timer(int nums[] ,int searched) {
 		double inicio = System.currentTimeMillis();
-		System.out.println("Se encuentra 15?");
-		BusquedaSecuencial(nums, 15);
+		System.out.println("Se encuentra "+ searched + " ? ");
+		System.out.println(BusquedaSecuencial(nums, searched));
 		double fin = System.currentTimeMillis();
 		System.out.println("Se demoro: "+ (fin-inicio)+"milisegundos");
 	}
-	private int[] crearArray() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+
 }
 
