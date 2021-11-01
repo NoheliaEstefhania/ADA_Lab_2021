bd5b9e6f3fa4a94e2ae633468858a7c97d3de869
NoheliaEstefhania <71722136+NoheliaEstefhania@users.noreply.github.com>
2021-11-01 13:02:41 -0500

    correccion de identacion

--- Aula_05/E011_Q11_TimeComplexity.java
+++ Aula_05/E011_Q11_TimeComplexity.java
@@ -27,16 +27,16 @@ public class E011_Q11_TimeComplexity {
 	//
 
 	public static void logarithmicMethod(int n) {	  							
-		int p = 0;//												   _				 ___	
-		for (int i = 1; i < n; i = i*2) {//								|O(log_2(n))	|	
-			p++;//														|				|
+		int p = 0;//							       _			     ___	
+		for (int i = 1; i < n; i = i*2) {//					|O(log_2(n))			|	
+			p++;//								|				|
 			System.out.println(" i = " + i + ", p = " + p);	 //	= O(1)	|				|
-		}   //														   _|				|			
-			//														   _				|
-		for (int j = 1; j < p; j = j*2) {//								|p = O(log_2(n))|Time complexity = O(log_2((log_2(n)))	
-			p++;//														|				|
-			System.out.println(" j = " + j + ", p = " + p);	 //	= O(1)	|O(log_2(p))	|		
-		}   //														   _|			  __|					
+		}   //								       _|				|			
+			//							       _				|
+		for (int j = 1; j < p; j = j*2) {//					|p = O(log_2(n))		|Time complexity = O(log_2((log_2(n)))	
+			p++;//								|				|
+			System.out.println(" j = " + j + ", p = " + p);	 //	= O(1)	|O(log_2(p))			|		
+		}   //								       _|			      __|					
 	}
 }	
 
