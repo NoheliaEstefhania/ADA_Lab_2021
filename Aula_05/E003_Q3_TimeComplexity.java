8ce831ca395695aa9c8d02e3b1783ef7ce184d95
NoheliaEstefhania <71722136+NoheliaEstefhania@users.noreply.github.com>
2021-10-31 23:19:28 -0500

    Q3 -  lineal time complexity example
    con = O(1) * 0(n/2)

---
+++ Aula_05/E003_Q3_TimeComplexity.java
@@ -0,0 +1,37 @@
+package Aula_05;
+
+import java.util.Scanner;
+
+/*
+ * Ejercicio - Q3
+ * Author: Nohelia Estefhania Tacca Apaza
+ * Description: Review of Time Complexity
+ * Question: What is the time complexity of method (parNumberList)?
+ * Answer: The time complexity is: O(n) --> lineal time 
+ * Date	  : 25/10/2021
+ */
+
+public class E003_Q3_TimeComplexity {
+
+	public static void main(String[] args) {
+		int n;
+		Scanner s = new Scanner(System.in);
+		System.out.println("Ingrese n:");
+		n = s.nextInt();
+
+		System.out.println("---------------------------");
+		System.out.println("Numeros enteros pares <= " + ((n)));
+		System.out.println("---------------------------");
+		parNumberList(n);
+	}
+	
+	//muestra los numeros pares menores o iguales a n 
+
+	public static void parNumberList(int n) {
+
+		for (int i = 0; i < n; i = i+2) {//		|O(n/2)	|= O(1) * 0(n/2)
+			//statement							|		|= O(n/2)
+			System.out.println(i);	 //	= O(1)	|		|= O(n)
+		}   //									|		|Time complexity = O(n)
+	}
+}
