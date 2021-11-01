4bd781ff0128b4c3615cdd8494f27f180b26d561
NoheliaEstefhania <71722136+NoheliaEstefhania@users.noreply.github.com>
2021-10-31 23:18:18 -0500

    Q2 - lineal time complexity example

---
+++ Aula_05/E002_Q2_TimeComplexity.java
@@ -0,0 +1,37 @@
+package Aula_05;
+
+import java.util.Scanner;
+
+/*
+ * Ejercicio - Q2
+ * Author: Nohelia Estefhania Tacca Apaza
+ * Description: Review of Time Complexity
+ * Question: What is the time complexity of method (numberListDesc)?
+ * Answer: The time complexity is: O(n) --> lineal time
+ * Date	  : 25/10/2021
+ */
+
+public class E002_Q2_TimeComplexity {
+
+	public static void main(String[] args) {
+		int n;
+		Scanner s = new Scanner(System.in);
+		System.out.println("Ingrese n:");
+		n = s.nextInt();
+
+		System.out.println("---------------------------");
+		System.out.println("Numeros enteros desde " + n + " hasta 1");
+		System.out.println("---------------------------");
+		numberListDesc(n);
+	}
+	
+	//muestra la lista de numeros desde 1 hasta n
+	
+	public static void numberListDesc(int n) {
+		
+		for (int i = n; i > 0; i--) {//			|O(n)	|= O(1) * 0(n)
+			//statement							|		|= O(n)			
+			System.out.println(i);	 //	= O(1)	|		|
+		}   //									|		|Time complexity = O(n)
+	}
+}
