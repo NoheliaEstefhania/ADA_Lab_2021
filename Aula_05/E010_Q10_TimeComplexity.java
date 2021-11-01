e7ed56c392666681752b90c8cbaa473d062a9e26
NoheliaEstefhania <71722136+NoheliaEstefhania@users.noreply.github.com>
2021-10-31 23:26:30 -0500

    Q10 - lineal time complexity example

---
+++ Aula_05/E010_Q10_TimeComplexity.java
@@ -0,0 +1,42 @@
+package Aula_05;
+
+import java.util.*;
+
+/*
+ * Ejercicio - Q10
+ * Author: Nohelia Estefhania Tacca Apaza
+ * Description: Review of Time Complexity
+ * Question: What is the time complexity of method (numberListAsc)?
+ * Answer: The time complexity is: O(n) --> lineal time 
+ * Date	  : 25/10/2021
+ */
+
+public class E010_Q10_TimeComplexity {
+
+	public static void main(String[] args) {
+		int n;
+		Scanner s = new Scanner(System.in);
+		System.out.println("Ingrese n:");
+		n = s.nextInt();
+
+		System.out.println("---------------------------");
+		System.out.println("Numeros enteros del 0 a " + (n-1) + " n veces");
+		System.out.println("---------------------------");
+		numberListAsc(n);
+	}
+	
+	//muestra la lista de numeros desde 0 hasta n-1, n veces 
+	
+	public static void numberListAsc(int n) {
+			//										   _						____		
+		for (int i = 0; i < n; i++) {//			|O(n)	|= O(1) * 0(n)				|
+			//statement							|		|= O(n)						|= O(n) + O(n)
+			System.out.println(i);	 //	= O(1)	|		|							|		
+		}   //									|	   _|Time complexity = O(n)		|= O(2n)
+			//										   _							|
+		for (int i = 0; i < n; i++) {//			|O(n)   |= O(1) * 0(n)				|= O(n)
+			//statement							|		|= O(n)						|
+			System.out.println(i);	 //	= O(1)	|		|							|Time complexity = O(n)
+		}   //									|	   _|Time complexity = O(n)	 ___|
+	}
+}
