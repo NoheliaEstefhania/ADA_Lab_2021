69526f75ab20959494208a7fc686bb1eb4593b48
NoheliaEstefhania <71722136+NoheliaEstefhania@users.noreply.github.com>
2021-10-31 23:30:50 -0500

    Q12 - correcciones de comentario

--- Aula_05/E012_Q12_TimeComplexity.java
+++ Aula_05/E012_Q12_TimeComplexity.java
@@ -19,7 +19,7 @@ public class E012_Q12_TimeComplexity {
 		n = s.nextInt();
 
 		System.out.println("-----------------------------");
-		System.out.println("Par de numeros entre 0 y " + ((n-1)));
+		System.out.println("Par de numeros entre 0 y " + ((n - 1)));
 		System.out.println("-----------------------------");
 		twoNumbers(n);
 	}
@@ -27,43 +27,41 @@ public class E012_Q12_TimeComplexity {
 	//
 
 	public static void twoNumbers(int n) {
-		//								  __						____
-		for (int i = 0; i < n; i++) {//		|= O(n)	  __				|
-			for (int j = 1; j < n; j = j*2) {//			|				|
-			//statement									|= O(log_2(n))	|= O(n) * O(log_2(n))	
-			System.out.println(i + " - " + j);	//= O(1)|				|= O(nlog_2(n))
-			}//										  __|				|
-		}   //							  __|						____|Time complexity = O(nlog(n))
-	}//																	
+		// __ ____
+		for (int i = 0; i < n; i++) {// |= O(n) __ | (1)
+			for (int j = 1; j < n; j = j * 2) {// | | (2)
+				// statement |= O(log_2(n)) |= O(n) * O(log_2(n)) (3)
+				System.out.println(i + " - " + j); // = O(1)| |= O(nlog_2(n))
+			} // __| |
+		} // __| ____|Time complexity = O(nlog(n))
+	}//
 }
 
 /*
-	(1) This repeats n times
-	(2) and this inner loop n times * log(n)
-	(3) and this statement n times * log(n) 
-	Adding them together n + 2 n * log(n)
-	O(n log(n))
- 
+ * (1) This repeats n times (2) and this inner loop n times * log(n) (3) and
+ * this statement n times * log(n) Adding them together n + 2 n * log(n)
+ * O(nlog(n))
+ * 
  */
 
-
-/* Review
-
-for (i = 0; i < n; i++)       // O(n)
-
-
-for (i = 0; i < n; i=i+2)     // O(n)
-
-
-for (i = n; i >= 1; i--)      // O(n)
-
-
-for (i = 1; i < n; i=i*2)     // O(log_2(n))
-
-
-for (i = 1; i < n; i=i*3)     // O(log_3(n))
-
-
-for (i = n; i >= 1; i=i/2)    // O(log_2(n))
-
-*/
+/*
+ * Review
+ * 
+ * for (i = 0; i < n; i++) // O(n)
+ * 
+ * 
+ * for (i = 0; i < n; i=i+2) // O(n)
+ * 
+ * 
+ * for (i = n; i >= 1; i--) // O(n)
+ * 
+ * 
+ * for (i = 1; i < n; i=i*2) // O(log_2(n))
+ * 
+ * 
+ * for (i = 1; i < n; i=i*3) // O(log_3(n))
+ * 
+ * 
+ * for (i = n; i >= 1; i=i/2) // O(log_2(n))
+ * 
+ */
