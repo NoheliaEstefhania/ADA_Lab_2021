d68c689dd6e225f8ffa03b0f18e95ebee5f0555c
NoheliaEstefhania <71722136+NoheliaEstefhania@users.noreply.github.com>
2021-11-15 15:49:41 -0500

    Borramos los comentarios innecesarios

--- Aula_06/Graphs/ListLinked.java
+++ Aula_06/Graphs/ListLinked.java
@@ -3,14 +3,14 @@ package Graphs;
 public class ListLinked<T> {
 	protected Node<T> first;
 
-	public ListLinked () {
+	public ListLinked() {
 		this.first = null;
 	}
-	
+
 	public boolean isEmpty() {
-		return this.first == null;	
+		return this.first == null;
 	}
-	
+
 	public T search(T data) { //le envìo el elemento
 		Node<T> nodo = this.first; //mi cabezala mantengo fija
 		while(nodo != null && !nodo.data.equals(data))//voy iterendo y moviéndome mientrs añun no encuentro mi elem
@@ -34,40 +34,22 @@ public class ListLinked<T> {
 			}
 			return r; 
 	}
-	
-	/*public void insertLast(T x) { //Tengo dos situaciones: lista vacia o lista con elementos
-		if(this.isEmpty()) //si mi lista està vacia tengo que hacer una inserciòn al inicio
-			this.insertFirst(x);
-		else { //llego al esle sabuendo que mi lista n està vacia
-			Node<T> lastNode = getLastNode(); //recorro hasta encontrar un elem nulo
-			lastNode.setNext(new Node<T>(x)); //creo nodo nuevo, el enlace que era null ahora apunta al nuevo nodo, el cual contiene al elem. x.
-		}
-	}
-	//precondiciòn: Fisrt in not empty
-	private Node<T> getLastNode(){ // ya se que mi lsta tiene elem.
-		Node<T> aux = this.first;
-		while (aux.getNext()!= null) { // me voy miviendo hasta encontra el elem que sea null
-			aux = aux.getNext(); // me muevo al sig. elem.
-		}
-		return aux;
-	}
-	*/
-	private T remove(T data) { //le envío el elemento
-        T item = null;
-        Node<T> aux = this.first;
-        if (this.first != null && this.first.data.equals(data)) {
-        	item = first.data;
-        	first = first.next;
- 
-        }else {
-        	while(aux.next != null && !aux.next.data.equals(data))
-        		aux = aux.getNext();
-        	if(aux.getNext() != null) {
-        		item = aux.next.data;
-        		aux.next = aux.next.next;
-        	}
-        }
-        return item;
-	}
 
+	private T remove(T data) {
+		T item = null;
+		Node<T> aux = this.first;
+		if (this.first != null && this.first.data.equals(data)) {
+			item = first.data;
+			first = first.next;
+
+		} else {
+			while (aux.next != null && !aux.next.data.equals(data))
+				aux = aux.getNext();
+			if (aux.getNext() != null) {
+				item = aux.next.data;
+				aux.next = aux.next.next;
+			}
+		}
+		return item;
+	}
 }
