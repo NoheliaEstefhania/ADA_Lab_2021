7f32d2b867fa9650613a3cfcd394861d7cfc0fe9
NoheliaEstefhania <71722136+NoheliaEstefhania@users.noreply.github.com>
2021-11-15 15:44:29 -0500

    Actualizaciòn de la clase Node

--- Aula_06/Graphs/Node.java
+++ Aula_06/Graphs/Node.java
@@ -1,27 +1,36 @@
 package Graphs;
 
+/*
+ * Nodo
+ */
+
 public class Node<T> {
 	protected T data;
-	protected Node<T> next; //puntero o referencia
-	
-	public Node (T data) {
+	protected Node<T> next;
+
+	public Node(T data) {
 		this.data = data;
 		this.next = null;
 	}
-	public Node (T data, Node<T> next) {
+
+	public Node(T data, Node<T> next) {
 		this.data = data;
 		this.next = next;
-		
+
 	}
+
 	public T getData() {
 		return this.data;
 	}
+
 	public Node<T> getNext() {
 		return next;
 	}
+
 	public void setData(T data) {
 		this.data = data;
 	}
+
 	public void setNext(Node<T> next) {
 		this.next = next;
 	}
