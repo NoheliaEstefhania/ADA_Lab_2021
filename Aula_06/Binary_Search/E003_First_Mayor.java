ea75190983683dd4bf3e1200dc7fc5626be99669
NoheliaEstefhania <71722136+NoheliaEstefhania@users.noreply.github.com>
2021-11-15 19:22:53 -0500

    Correcciòn del ejercicio E003

--- Aula_06/Binary_Search/E003_First_Mayor.java
+++ Aula_06/Binary_Search/E003_First_Mayor.java
@@ -14,24 +14,22 @@ public class E003_First_Mayor {
 	public static void main(String[] arg) {
 		Scanner s = new Scanner(System.in);
 		int searched, position;
-		int[] nums = {1,2,3,4,5,6,7,8,9,10};
+		int[] nums = {5,10,15,20,25,30,35,40,45,50};
 		System.out.println("Ingrese el número que esté buscando:");
 		searched = s.nextInt();
 		position = busquedaBinaria(nums, searched);
-		if (position == -1)
-			System.out.println("No se encontró");
-		else
-			System.out.println("El primer número mayor a " + searched + " está en la posición : " + position);
+		
+		System.out.println("El primer número mayor o igual es : " + nums[position]);
 	}
-	//devuelve la posición del siguiente número mayor que se pone en la búsqueda
+	//devuelve la posición del siguiente número mayor que se pone en la búsqueda o el mismo
 	private static int busquedaBinaria(int[] nums, int searched1) {
-		int alta, baja, media;
+		int alta, baja, media = 0;
 		baja = 0; //L
 		alta = (nums.length - 1); //R
 		while (baja <= alta) {
 			media = (baja + ((alta - baja) / 2));
 			if (nums[media] == searched1) {
-				return nums[media + 1];
+				return media;
 			} else if (nums[media] < searched1) {
 				baja = media + 1;
 
@@ -39,7 +37,7 @@ public class E003_First_Mayor {
 				alta = media - 1;
 			}
 		}
-		return -1;
+		return media;
 	}
 
 }
