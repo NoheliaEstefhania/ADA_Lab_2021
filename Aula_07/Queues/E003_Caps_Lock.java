4847cf2db26bf78861e38207523e0a1c3d810adb
NoheliaEstefhania <71722136+NoheliaEstefhania@users.noreply.github.com>
2021-11-19 18:40:30 -0500

    E003 Eniciación del ejercicio Queue

---
+++ Aula_07/Queues/E003_Caps_Lock.java
@@ -0,0 +1,14 @@
+package Queues;
+
+/*
+ * Ejercicio	: E001
+ * Author		: Nohelia Estefhania Tacca Apaza
+ * Description	: Caps Lock
+ * Details		: 
+
+ * Date	  		: 15/11/2021
+ */
+
+public class E003_Caps_Lock {
+
+}
