c079b6f6cfd0648a6fd7ddfac9cdf77731665173
NoheliaEstefhania <71722136+NoheliaEstefhania@users.noreply.github.com>
2021-11-22 00:06:14 -0500

    se agregò el método de capsLock Y su respectiva prueba

--- Aula_07/Queues/E003_Caps_Lock.java
+++ Aula_07/Queues/E003_Caps_Lock.java
@@ -1,14 +1,57 @@
 package Queues;
 
+import java.util.*;
+
 /*
- * Ejercicio	: E001
+ * Ejercicio	: E003
  * Author		: Nohelia Estefhania Tacca Apaza
  * Description	: Caps Lock
- * Details		: 
-
+ * Details		: You are sending  a  message  to a friend  with a  special  keyboard.  
+ * 				  The keyboard only  accepts lowercase  letters,  and stores  them  in a  buffer.
+				  When the  @ key is  pressed,  it  toggles the  CapsLock key:  all  letters 
+				  in the  buffer have their  case flipped,  as  well  as  all  subsequent  keypresses.
+ 				  When the  $  key is  pressed,  the  entire  buffer is  flushed.
  * Date	  		: 15/11/2021
  */
 
 public class E003_Caps_Lock {
+	public static void main(String[] args) {
+		String txt = "abc$d@ef$@g$";
+		System.out.println(txt);
+		capsLock(txt);
+	}
 
+	public static void capsLock(String txt) {
+		Queue<Character> queue = enQueue(txt);
+		
+		String message = "";
+		String buffer = "";
+		
+		boolean toUpperCase = false;
+		
+		while(!queue.isEmpty()) {
+			Character letter = queue.poll();
+			if(letter == '$') {
+				if(toUpperCase) {
+					buffer = buffer.toUpperCase();
+				} 
+				message += buffer;
+				buffer = "";
+				
+			} else if(letter == '@') { 
+				toUpperCase = !toUpperCase;
+			} else {
+				buffer += letter;
+			}
+		}
+		System.out.print(message);
+	}
+
+	public static Queue<Character> enQueue(String txt) {
+		Queue<Character> queue = new LinkedList<Character>();
+		for(Character c : txt.toCharArray()) {
+			queue.add(c);
+		}
+		return queue;
+	}
 }
