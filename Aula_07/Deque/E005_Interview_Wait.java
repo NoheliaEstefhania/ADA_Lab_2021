5a672ef8d60164f6c3a95ffcf0ae570fb9c0ccd3
NoheliaEstefhania <71722136+NoheliaEstefhania@users.noreply.github.com>
2021-11-22 00:44:27 -0500

    E005 métodos y prueba

--- Aula_07/Deque/E005_Interview_Wait.java
+++ Aula_07/Deque/E005_Interview_Wait.java
@@ -6,38 +6,39 @@ import java.util.*;
  * Ejercicio	: E005
  * Author		: Nohelia Estefhania Tacca Apaza
  * Description	: Deque
- * Details		: 
- 			  	
+ * Details		: You are sitting  on a row  of chairs,  waiting  for an interview. 
+ 			  	  When choosing the  next  interviewee,  the  interviewer  will  always choose  from  
+ 			  	  either  the  very left  side  or the  very right  side  of the  row  of chairs.
  * Date	  		: 15/11/2021
  */
 
 public class E005_Interview_Wait {
 	public static void main (String[]args){
-		Deque<Integer> myDeque = new LinkedList<Integer>();	
-		//caso de prueba visto en clase
+		Deque<Integer> deque = new LinkedList<Integer>();	
+		
 		int [] array = {4,-1,5,2,3};
 		for(int i = 0; i < array.length; i++){
-			myDeque.addLast(array[i]);
+			deque.addLast(array[i]);
 		}
-		//imprimimos el tiempo esperado
-		System.out.println("Cantidad de tiempo: " + interviewVait(myDeque));
+		System.out.println("Ouput : " + interviewVait(deque));
 	}
 
-	private static int interviewVait(Deque<Integer> myDeque){
-		// Encolamos los tiempos en una cola doble
-		System.out.println("Cola de entrevistados: " + myDeque);
-		int acumulado = 0;
+	private static int interviewVait(Deque<Integer> deque){
+		
+		System.out.println("Input : " + deque);
+		
+		int sms = 0;
 		while(true){
-			if(myDeque.peek() == -1 || myDeque.peekLast() == -1){
-				break;//yo soy -1, rompe pues me toca ser entrevistado
+			if(deque.peek() == -1 || deque.peekLast() == -1){
+				break;
 			}
-			if(myDeque.peek() < myDeque.peekLast()){//extremos fin
-				acumulado += myDeque.pollFirst();//extremos inicio
+			if(deque.peek() < deque.peekLast()){
+				sms += deque.pollFirst();
 			}
 			else {
-				acumulado += myDeque.pollLast();
+				sms += deque.pollLast();
 			}
 		}
-		return acumulado;
+		return sms;
 	}
 }
