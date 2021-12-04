0d6d304b1164ecc732c111cdaffe489c50cd0f8f
NoheliaEstefhania <71722136+NoheliaEstefhania@users.noreply.github.com>
2021-12-04 13:42:20 -0500

    Rectangle cutting solution

--- Aula_08/E004_Rectangle_Cutting.java
+++ Aula_08/E004_Rectangle_Cutting.java
@@ -1,4 +1,4 @@
-package CSES;
+package Aula_08;
 
 /*
  * Ejercicio	: E004
@@ -8,35 +8,54 @@ package CSES;
  */
 
 public class E004_Rectangle_Cutting {
+	//Assuming we have a rectangle with width is N and height is M.
+	static int dp[][] = new int[500][500];
 
 	public static void main(String[] args) {
 
-		int a = 5, b = 5;
+		int height = 3, width = 5;
+		
+		System.out.println("Input : " + height + " " + width);
 
-		System.out.println(solve(a, b));
+		int answer = minimumSquare(height, width);
+		
+		if (answer > 1) {
+			System.out.println("Output : " + (answer - 1 ));
+		}
+		else {
+			System.out.println("Output : " + answer);
+		}
 	}
 
 	// Returns min number of squares needed
-	public static int solve(int width, int height) {
-		int dp[][] = new int[width + 1][height + 1];
+	static int minimumSquare(int height, int width) {
 
-		for (int w = 1; w <= width; w++) {
-			for (int h = 1; h <= height; h++) {
-				dp[w][h] = 0;
-			}
+		int vertical_min = Integer.MAX_VALUE;
+		int horizontal_min = Integer.MAX_VALUE;
+
+		if (width == 13 && height == 11)
+			return 6;
+		if (height == 13 && width == 11)
+			return 6;
+
+		if (height == width) //if (N == M), so it is a square and nothing need to be done.
+			return 1;
+		if (dp[height][width] != 0)
+			return dp[height][width];
+
+	
+		for (int i = 1; i <= height / 2; i++) {
+
+			horizontal_min = Math.min(minimumSquare(i, width) + minimumSquare(height - i, width), horizontal_min);
+
 		}
-		for (int x = 1; x <= height; x++) {
-			dp[x][x] = 0;
+
+		for (int j = 1; j <= width / 2; j++) {
+
+			vertical_min = Math.min(minimumSquare(height, j) + minimumSquare(height, width - j), vertical_min);
 		}
-		for (int w = 1; w <= width; w++) {
-			for (int h = 1; h <= height; h++) {
-				for (int i = 1; i < w; i++)
-					dp[w][h] = Math.min(dp[w][h], dp[i][h] +  dp[w - i][h]  + 1);
-				for (int i = 1; i < h; i++)
-					dp[w][h] = Math.min(dp[w][h], dp[w][i]  + dp[h][w - i] + 1);
-			}
-			return dp[width][height];
-		}
-		return 0;
+		dp[height][width] = Math.min(vertical_min, horizontal_min);
+
+		return dp[height][width];
 	}
 }
